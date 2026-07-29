c919ed88833f4e4a9aa2690e7d2f69f68f0ce025
damon <damon@test.t>
2026-07-28 22:59:06 -0700

    存储过程初始化脚本fix

--- document/psql/vastbase/final/000_run_all.sql
+++ document/psql/vastbase/final/000_run_all.sql
@@ -1,38 +1,38 @@
 \echo [1/13] loading 001_etl_init.sql
-\i /root/work-project/project-02/gk-data-analysis/document/psql/vastbase/final/001_etl_init.sql
+\ir 001_etl_init.sql
 
 \echo [2/13] loading 002_ods_init.sql
-\i /root/work-project/project-02/gk-data-analysis/document/psql/vastbase/final/002_ods_init.sql
+\ir 002_ods_init.sql
 
 \echo [3/13] loading 003_report_init.sql
-\i /root/work-project/project-02/gk-data-analysis/document/psql/vastbase/final/003_report_init.sql
+\ir 003_report_init.sql
 
 \echo [4/13] loading 004_edw_init.sql
-\i /root/work-project/project-02/gk-data-analysis/document/psql/vastbase/final/004_edw_init.sql
+\ir 004_edw_init.sql
 
 \echo [5/13] loading 005_visual_screen_init.sql
-\i /root/work-project/project-02/gk-data-analysis/document/psql/vastbase/final/005_visual_screen_init.sql
+\ir 005_visual_screen_init.sql
 
 \echo [6/13] loading 006_indicators_lib_init.sql
-\i /root/work-project/project-02/gk-data-analysis/document/psql/vastbase/final/006_indicators_lib_init.sql
+\ir 006_indicators_lib_init.sql
 
 \echo [7/13] loading 007_events_init.sql
-\i /root/work-project/project-02/gk-data-analysis/document/psql/vastbase/final/007_events_init.sql
+\ir 007_events_init.sql
 
 \echo [8/13] loading 008_ucloud_tables_init.sql
-\i /root/work-project/project-02/gk-data-analysis/document/psql/vastbase/final/008_ucloud_tables_init.sql
+\ir 008_ucloud_tables_init.sql
 
 \echo [9/13] loading 009_upm_alarmlog_tables_init.sql
-\i /root/work-project/project-02/gk-data-analysis/document/psql/vastbase/final/009_upm_alarmlog_tables_init.sql
+\ir 009_upm_alarmlog_tables_init.sql
 
 \echo [10/13] loading 010_upm_system_data_tables_init.sql
-\i /root/work-project/project-02/gk-data-analysis/document/psql/vastbase/final/010_upm_system_data_tables_init.sql
+\ir 010_upm_system_data_tables_init.sql
 
 \echo [11/13] loading 011_upm_netperformance_tables_init.sql
-\i /root/work-project/project-02/gk-data-analysis/document/psql/vastbase/final/011_upm_netperformance_tables_init.sql
+\ir 011_upm_netperformance_tables_init.sql
 
 \echo [12/13] loading 012_ucloud_upm_procedures_init.sql
-\i /root/work-project/project-02/gk-data-analysis/document/psql/vastbase/final/012_ucloud_upm_procedures_init.sql
+\ir 012_ucloud_upm_procedures_init.sql
 
 \echo [13/13] loading 013_adm_tables_init.sql
-\i /root/work-project/project-02/gk-data-analysis/document/psql/vastbase/final/013_adm_tables_init.sql
+\ir 013_adm_tables_init.sql
